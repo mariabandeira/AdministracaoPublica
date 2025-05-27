61f925bcac8ca3b46b5aa31394ef1a674170d2e7
Maria Eduarda Bandeira <125301815+mariabandeira@users.noreply.github.com>
2025-05-27 20:11:41 -0300

    feat: adicionando coluna anual e tirando o drop duplicates

--- CadUnico/BasesRN/clean_bases_rn.py
+++ CadUnico/BasesRN/clean_bases_rn.py
@@ -5,14 +5,17 @@ cadunico_rn = pd.read_csv("cadunico_rn_combined.csv", sep=';', low_memory=False)
 # removendo colunas que não se encontram em um formato adequado para o treinamento de ML
 cadunico_rn = cadunico_rn.drop(columns=['dat_cadastramento_fam', 'dat_alteracao_fam', 'dat_atualizacao_familia', 'nom_estab_assist_saude_fam', 'cod_eas_fam', 'nom_centro_assist_fam', 'cod_centro_assist_fam', 'ind_parc_mds_fam'])
 
-# removendo linhas duplicadas
-cadunico_rn = cadunico_rn.drop_duplicates(subset=['id_familia'], keep='last')
-
 # colunas que precisam de tratamento de tipos
 columns_to_convert = ['vlr_renda_media_fam', 'peso.fam']
 
 for column in columns_to_convert:
     cadunico_rn[column] = cadunico_rn[column].replace(',', '.', regex=True).astype(float)
+
+repetidos_por_ano = (
+    cadunico_rn.groupby(['ano', 'id_familia'])
+    .size()
+    .reset_index(name='contagem')
+)
 
 # removendo linhas com valores ausentes
 cadunico_rn = cadunico_rn.dropna()
--- CadUnico/BasesRN/concat_bases_rn.py
+++ CadUnico/BasesRN/concat_bases_rn.py
@@ -1,17 +1,14 @@
 import pandas as pd
 
-cadUnico_rn_2012 = pd.read_csv("cadunico_rn_2012.csv", sep=';', low_memory=False)
-cadUnico_rn_2013 = pd.read_csv("cadunico_rn_2013.csv", sep=';', low_memory=False)
-cadUnico_rn_2014 = pd.read_csv("cadunico_rn_2014.csv", sep=';', low_memory=False)
-cadUnico_rn_2015 = pd.read_csv("cadunico_rn_2015.csv", sep=';', low_memory=False)
-cadUnico_rn_2016 = pd.read_csv("cadunico_rn_2016.csv", sep=';', low_memory=False)
-cadUnico_rn_2017 = pd.read_csv("cadunico_rn_2017.csv", sep=';', low_memory=False)
-cadUnico_rn_2018 = pd.read_csv("cadunico_rn_2018.csv", sep=';', low_memory=False)
+anos = range(2012, 2019)
+bases_rn = {}
+
+for ano in anos:
+    df = pd.read_csv(f"cadunico_rn_{ano}.csv", sep=';', low_memory=False)
+    df["ano"] = ano
+    bases_rn[ano] = df
 
 # Mesclando os arquivos CSV em um único DataFrame
-cadunico_rn_combined = pd.concat([cadUnico_rn_2012, cadUnico_rn_2013, cadUnico_rn_2014, 
-                              cadUnico_rn_2015, cadUnico_rn_2016, cadUnico_rn_2017, cadUnico_rn_2018], 
-                              ignore_index=True)
-
+cadunico_rn_combined = pd.concat(bases_rn.values(), ignore_index=True)
 
 cadunico_rn_combined.to_csv('cadunico_rn_combined.csv', index=False, sep=';')
