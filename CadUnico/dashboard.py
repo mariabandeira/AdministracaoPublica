8fc4bf8882f589ba6e88d8409e42c92f8a38bdf7
mariabandeira <maria.bandeira@academico.ufpb.br>
2025-07-07 11:35:05 -0300

    feat: filtro do mapa para cidade selecionada

--- CadUnico/dashboard.py
+++ CadUnico/dashboard.py
@@ -9,6 +9,14 @@ import plotly.express as px
 from shapely.geometry import shape, Point
 import os
 from dotenv import load_dotenv
+from shapely.geometry import shape
+import locale
+
+# Defina o locale para português do Brasil
+locale.setlocale(locale.LC_ALL, 'pt_BR.UTF-8')
+
+if 'cd_ibge_mapa' not in st.session_state:
+    st.session_state['cd_ibge_mapa'] = None
 
 st.markdown("""
     <style>
@@ -46,11 +54,27 @@ cadunico = pd.read_csv(data_path, sep=';', low_memory=False)
 cadunico = cadunico[cadunico['ano'].isin(range(2012, 2019))]
 geojson = requests.get(geojson_url).json()
 
-nomes_cidades = sorted([feature['properties']['name'] for feature in geojson['features']])
+nomes_cidades = sorted(
+    [feature['properties']['name'] for feature in geojson['features']],
+    key=locale.strxfrm
+)
 nomes_cidades.insert(0, "Todos os municípios")
 
 selected_year = st.sidebar.selectbox('Selecione o ano', list(range(2012, 2019)), index=6)
-selected_city = st.sidebar.selectbox('Selecione a cidade', nomes_cidades, index=0)
+
+if "update_city_from_map" in st.session_state and st.session_state.update_city_from_map:
+    # Atualize o valor do dropdown antes de criar o widget
+    selected_city = st.session_state.city_from_map
+    st.session_state.update_city_from_map = False
+else:
+    selected_city = st.session_state.get("selected_city", "Todos os municípios")
+
+selected_city = st.sidebar.selectbox(
+    'Selecione a cidade',
+    nomes_cidades,
+    index=nomes_cidades.index(selected_city) if selected_city in nomes_cidades else 0,
+    key="selected_city"
+)
 pagina = st.sidebar.radio("Selecione a página", ["Mapa e Indicadores", "Características Domiciliares", "Predição com ML"])
 
 if selected_city != "Todos os municípios":
@@ -69,20 +93,45 @@ def encontrar_municipio(lat, lon, geojson):
             return feature['properties']['name'], feature['properties']['id']
     return None, None
 
-def exibir_mapa():
+def filtrar_geojson_por_cidade(geojson, cd_ibge):
+    if not cd_ibge:
+        return geojson
+    features = [f for f in geojson['features'] if f['properties']['id'] == cd_ibge]
+    return {'type': 'FeatureCollection', 'features': features}
+
+def get_centro_municipio(geojson, cd_ibge):
+    for feature in geojson['features']:
+        if feature['properties']['id'] == cd_ibge:
+            geom = shape(feature['geometry'])
+            lon, lat = geom.centroid.xy
+            return [lat[0], lon[0]]
+    # Valor padrão (RN)
+    return [-7, -36.5]
+
+
+def exibir_mapa(cd_ibge=None):
     dados_agrupados = cadunico.groupby(['cd_ibge', 'ano'], as_index=False)['marc_pbf'].sum()
     df_selected_year = dados_agrupados[dados_agrupados.ano == selected_year]
     df_selected_year['cd_ibge'] = df_selected_year['cd_ibge'].astype(str)
     marc_pbf_dict = df_selected_year.set_index('cd_ibge')['marc_pbf'].to_dict()
 
-    for feature in geojson['features']:
-        cd_ibge = feature['properties']['id']
-        valor = marc_pbf_dict.get(cd_ibge, 0)
+    geojson_filtrado = filtrar_geojson_por_cidade(geojson, cd_ibge)
+    if cd_ibge:
+        df_selected_year = df_selected_year[df_selected_year['cd_ibge'] == str(cd_ibge)]
+        centro = get_centro_municipio(geojson, cd_ibge)
+        zoom = 10  # Ajuste o nível de zoom conforme necessário
+    else:
+        centro = [-7, -36.5]
+        zoom = 7
+
+    for feature in geojson_filtrado['features']:
+        cd_ibge_f = feature['properties']['id']
+        valor = marc_pbf_dict.get(cd_ibge_f, 0)
         feature['properties']['marc_pbf'] = valor
 
-    m = folium.Map(location=[-7, -36.5], zoom_start=7)
+    m = folium.Map(location=centro, zoom_start=zoom)
     choropleth = folium.Choropleth(
-        geo_data=geojson,
+        geo_data=geojson_filtrado,
         name='choropleth',
         data=df_selected_year,
         columns=['cd_ibge', 'marc_pbf'],
@@ -118,7 +167,7 @@ def exibir_mapa():
         """)
 
         st.markdown("""
-        ### Como funciona o programa?
+        #### Como funciona o programa?
         - **Critérios de elegibilidade**: Famílias com renda per capita abaixo de um determinado valor.
         - **Objetivos**: Erradicação da pobreza, inclusão social e melhoria nas condições de vida das famílias vulneráveis.
         """)
@@ -137,10 +186,38 @@ CB_color_cycle = ['#377eb8', '#4daf4a', '#e41a1c', '#dede00', '#a65628','#999999
 
 # Página 1 - Mapa e Indicadores
 if pagina == "Mapa e Indicadores":
-    mapa = exibir_mapa()
-    municipio_nome, cd_ibge_final = get_municipio_selecionado(mapa, selected_city, cd_ibge_selected_sidebar)
+    st.subheader(f"Mapa e Indicadores - {selected_year}") 
+
+    # Atualiza o cd_ibge_mapa conforme o dropdown
+    if st.session_state.selected_city != "Todos os municípios":
+        cd_ibge_selected = next(
+            (f['properties']['id'] for f in geojson['features'] if f['properties']['name'] == st.session_state.selected_city),
+            None
+        )
+        st.session_state['cd_ibge_mapa'] = cd_ibge_selected
+    else:
+        st.session_state['cd_ibge_mapa'] = None
+
+    # Exibe o mapa filtrado
+    mapa = exibir_mapa(st.session_state['cd_ibge_mapa'])
+
+    # Se clicou no mapa, atualize o session_state e o dropdown
+    click_data = mapa.get('last_clicked')
+    if click_data:
+        municipio_nome, cd_ibge_final = encontrar_municipio(click_data['lat'], click_data['lng'], geojson)
+        if cd_ibge_final:
+            st.session_state['cd_ibge_mapa'] = cd_ibge_final
+            # Atualiza o dropdown para o nome da cidade clicada, usando flags auxiliares
+            st.session_state.city_from_map = municipio_nome
+            st.session_state.update_city_from_map = True
+            st.rerun()
+    else:
+        municipio_nome = st.session_state.selected_city if st.session_state.selected_city != "Todos os municípios" else None
+        cd_ibge_final = st.session_state['cd_ibge_mapa']
+
+    #  cd_ibge_final para filtrar os dados
     if municipio_nome:
-        st.subheader(f"Município selecionado: {municipio_nome}")
+        st.subheader(f"Município selecionado: {municipio_nome} - {estado}")
 
     if cd_ibge_final:
         cadunico_filtrado = cadunico[cadunico['cd_ibge'].astype(str) == str(cd_ibge_final)]
@@ -172,12 +249,38 @@ if pagina == "Mapa e Indicadores":
 
 # Página 2 - Gráficos de Características Domiciliares
 if pagina == "Características Domiciliares":
-    mapa = exibir_mapa()
-    municipio_nome, cd_ibge_final = get_municipio_selecionado(mapa, selected_city, cd_ibge_selected_sidebar)
-    if municipio_nome:
-        st.subheader(f"Município selecionado: {municipio_nome}")
-
     st.subheader(f"Características dos Domicílios - {selected_year}")
+
+    # Atualiza o cd_ibge_mapa conforme o dropdown
+    if st.session_state.selected_city != "Todos os municípios":
+        cd_ibge_selected = next(
+            (f['properties']['id'] for f in geojson['features'] if f['properties']['name'] == st.session_state.selected_city),
+            None
+        )
+        st.session_state['cd_ibge_mapa'] = cd_ibge_selected
+    else:
+        st.session_state['cd_ibge_mapa'] = None
+
+    # Exibe o mapa filtrado
+    mapa = exibir_mapa(st.session_state['cd_ibge_mapa'])
+
+    # Se clicou no mapa, atualize o session_state e o dropdown
+    click_data = mapa.get('last_clicked')
+    if click_data:
+        municipio_nome, cd_ibge_final = encontrar_municipio(click_data['lat'], click_data['lng'], geojson)
+        if cd_ibge_final:
+            st.session_state['cd_ibge_mapa'] = cd_ibge_final
+            # Atualiza o dropdown para o nome da cidade clicada, usando flags auxiliares
+            st.session_state.city_from_map = municipio_nome
+            st.session_state.update_city_from_map = True
+            st.rerun()
+    else:
+        municipio_nome = st.session_state.selected_city if st.session_state.selected_city != "Todos os municípios" else None
+        cd_ibge_final = st.session_state['cd_ibge_mapa']
+
+
+    if municipio_nome:
+        st.subheader(f"Município selecionado: {municipio_nome} - {estado}")
 
     if cd_ibge_final:
         cad_filtro = cadunico[(cadunico['cd_ibge'] == int(cd_ibge_final)) & (cadunico['ano'] == selected_year)]
@@ -193,18 +296,67 @@ if pagina == "Características Domiciliares":
         "Espécie do domicílio": 'cod_especie_domic_fam'
     }
 
-    for coluna in colunas_categorias.values():
-        cad_filtro[coluna] = cad_filtro[coluna].astype(str)
+    mapeamentos = {
+        'cod_destino_lixo_domic_fam': {
+            1: "Coletado diretamente",
+            2: "Coletado indiretamente",
+            3: "Queimado ou enterrado",
+            4: "Jogado em terreno baldio",
+            5: "Jogado em rio ou mar",
+            6: "Outro destino"
+        },
+        'cod_iluminacao_domic_fam': {
+            1: "Elétrica com medidor próprio",
+            2: "Elétrica com medidor comunitário",
+            3: "Elétrica sem medidor",
+            4: "Óleo, querosene ou gás",
+            5: "Vela",
+            6: "Outra forma"
+        },
+        'cod_calcamento_domic_fam': {
+            1: "Total",
+            2: "Parcial",
+            3: "Não existe"
+        },
+        'cod_abaste_agua_domic_fam': {
+            1: "Rede geral",
+            2: "Poço ou nascente",
+            3: "Cisterna",
+            4: "Outra forma"
+        },
+        'cod_escoa_sanitario_domic_fam': {
+            1: "Rede coletora",
+            2: "Fossa séptica",
+            3: "Fossa rudimentar",
+            4: "Vala a céu aberto",
+            5: "Direto para um rio, lago ou mar",
+            6: "Outra forma"
+        },
+        'cod_especie_domic_fam': {
+            1: "Particular Permanente",
+            2: "Particular improvisado",
+            3: "Coletivo"
+        }
+    }
+
+    for coluna, mapeamento in mapeamentos.items():
+        if coluna in cad_filtro.columns:
+            cad_filtro[coluna] = cad_filtro[coluna].replace(mapeamento)
 
     col1, col2, col3 = st.columns(3)
     for i, (titulo, coluna) in enumerate(colunas_categorias.items()):
         graf = px.histogram(cad_filtro, x=coluna, title=titulo, color=coluna)
         graf.update_layout(height=400, width=500, margin=dict(t=50, b=40), legend_title_text="")
+        graf.update_xaxes(title=None)
+        graf.update_yaxes(title=None) 
         [col1, col2, col3][i % 3].plotly_chart(graf, use_container_width=True)
 
     col1, col2 = st.columns(2)
     with col1:
         fig_comodos = px.histogram(cad_filtro, x='qtd_comodos_domic_fam', title='Qtd. de cômodos do domicílio')
+        fig_comodos.update_layout(legend_title_text="")
+        fig_comodos.update_xaxes(title=None)
+        fig_comodos.update_yaxes(title=None)
         st.plotly_chart(fig_comodos, use_container_width=True)
     with col2:
         banheiros = cad_filtro['cod_banheiro_domic_fam'].value_counts().rename({1: 'Sim', 2: 'Não'})
